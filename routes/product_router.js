77bfc8775428dd462c65086d28b759da89e76a21
h1008h1008 <103942745+h1008h1008@users.noreply.github.com>
2024-01-02 19:59:38 +0800

    Revert "backstage"
    This reverts commit 78a7f1fb8d730fe3c0e86fc6374c8fe2d01808a4.

--- routes/product_router.js
+++ routes/product_router.js
@@ -1,13 +1,11 @@
 const { Router } = require('express')
 const product_controller = require('../controllers/product_controller.js')
 const product_router = Router()
-const {  validateadminToken} = require("../service/JWT.js");
 
 product_router.get('/', product_controller.renderIndexPageHandler)
 product_router.get('/shoppingcar', product_controller.renderShooppingPageHandler)
 product_router.get('/product/:productId', product_controller.renderProductPageHandler)
-product_router.get('/admin', validateadminToken , product_controller.returnadminHandler)
-product_router.get('/admin/:productId',validateadminToken, product_controller.returneditHandler)
+
 
 module.exports = product_router
 
